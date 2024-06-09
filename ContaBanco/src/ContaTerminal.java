2ccf0bbe6a623b1eae6a2332914cd16637f1d816
pedro parente <plucas14252931@gmail.com>
2024-06-09 12:27:36 -0300

    Adicionado a entrada e saidas dos dados da conta.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        int numeroDaAgencia = scanner.nextInt();
+
+        System.out.println("Por favor, informe sua Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, informe o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numeroDaAgencia + " e seu saldo " + saldo + " R$ já está disponível para saque.");
+        
     }
-}
+} 
